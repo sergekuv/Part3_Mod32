eabfb839dae73bba7db5062026802ca2dd65aaef
Sergey <sergekuv@yandex.ru>
2022-04-17 20:39:18 +0300

    Before Task 32.11.1

--- MvcStartApp/Models/Db/BlogRepository.cs
+++ MvcStartApp/Models/Db/BlogRepository.cs
@@ -9,7 +9,7 @@ namespace MvcStartApp.Models.Db
     public interface IBlogRepository
     {
         public Task AddUser(User user);
-        public void AddUserSync(User user);
+        //public void AddUserSync(User user);
 
         Task<User[]> GetUsers();
 
@@ -23,24 +23,39 @@ namespace MvcStartApp.Models.Db
             _context = context;
         }
 
-        public async Task AddUser(User user)
-        {
-            var entry = _context.Entry(user);
-            if (entry.State == EntityState.Detached)
-                await _context.Users.AddAsync(user);
-            await _context.SaveChangesAsync();
-        }
-        public void AddUserSync(User user)
-        {
-            var entry = _context.Entry(user);
-            if (entry.State == EntityState.Detached)
-                _context.Users.Add(user);
-            _context.SaveChanges();
-        }
+        //public async Task AddUser(User user)
+        //{
+        //    var entry = _context.Entry(user);
+        //    if (entry.State == EntityState.Detached)
+        //        await _context.Users.AddAsync(user);
+        //    await _context.SaveChangesAsync();
+        //}
+        //public void AddUserSync(User user)
+        //{
+        //    var entry = _context.Entry(user);
+        //    if (entry.State == EntityState.Detached)
+        //        _context.Users.Add(user);
+        //    _context.SaveChanges();
+        //}
 
         public async Task<User[]> GetUsers()
         {
             return await _context.Users.ToArrayAsync();
         }
+
+        public async Task AddUser(User user)
+        {
+            user.JoinDate = DateTime.Now;
+            user.Id = Guid.NewGuid();
+
+            // Добавление пользователя
+            var entry = _context.Entry(user);
+            if (entry.State == EntityState.Detached)
+                await _context.Users.AddAsync(user);
+
+            // Сохранение изенений
+            await _context.SaveChangesAsync();
+        }
     }
+
 }
